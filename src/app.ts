e410d0932e9e561ccf32761a93c04539b04ed87b
OsoTega <96957781+OsoTega@users.noreply.github.com>
2024-02-10 19:13:29 +0000

    update commit

--- src/app.ts
+++ src/app.ts
@@ -64,10 +64,6 @@ const findAvailableUser = (rooms: string[])=>{
     if(waitingRooms.length > 0){
         index = Math.floor(Math.random() * (waitingRooms.length-1));
     }
-    waitingRooms.map((room)=>{
-        console.log(room.room)
-    });
-    console.log(waitingRooms.length);
     // for(let i = 0; i < activeRooms.length; i++){
     //     if(activeRooms[i].active === false && !rooms.includes(activeRooms[i].room) && activeRooms[i].users.length < 2){
     //         index = i;
@@ -136,10 +132,7 @@ app.post("/leave-room", (req, res)=>{
             const chatRoom = activeRooms[index];
             waitingRooms.push(chatRoom);
         }
-        waitingRooms.map((room)=>{
-            console.log(room.room)
-        });
-        console.log(waitingRooms.length);
+
         activeRooms.splice(index, 1);
         res.send(JSON.stringify({success: true}))
     }catch(e){
